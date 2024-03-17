ff7b9bacbdb3a440e5917662ae72a0e64ce3da2a
❤️ Muhammad Reza Fahlevi 🌺 <muhdrezafahlevi@gmail.com>
2024-03-17 15:36:13 +0800

    Fix bug 'Win/Windows' key stays highlighted when released.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+const metaLeftKeyElement = document.querySelector('[data-key="MetaLeft"]');
+
 function highlight(dataset, key, action) {
     const element = document.querySelector(`[${dataset}="${key}"]`);
+    key === "MetaLeft" ? isMetaKeyPreseed = true : isMetaKeyPreseed = false;
+
+    // Fix code for "Win/Windows" key staying highlighted when released.
+    if (metaLeftKeyElement.classList.contains('active')) {
+        metaLeftKeyElement.classList.remove('active');
+    }
 
     if (element) {
         action === "add" ? element.classList.add('active') : element.classList.remove('active');
